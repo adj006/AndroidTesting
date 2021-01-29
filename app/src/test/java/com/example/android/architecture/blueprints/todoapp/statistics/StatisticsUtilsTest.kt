f097617847307a487c9c5536cb719352fc46f0b8
Adrian <ajim1989@gmail.com>
2021-01-29 15:35:18 -0800

    Created more tests for getActiveAndCompletedStats()

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -16,11 +16,29 @@ class StatisticsUtilsTest {
         val tasks = listOf<Task>(
                 Task("Title", "Description", isCompleted = false)
         )
-
+        // When the list of tasks is computed with a completed task
         val result = getActiveAndCompletedStats(tasks)
 
+        // Then the percentages are 0 and 100
         assertEquals(0f, result.completedTasksPercent)
         assertEquals(100f, result.activeTasksPercent)
+    }
+
+    /*
+    If there's 1 completed task and no active task,
+    then there are 0% active tasks and 100% completed tasks.
+     */
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsHundredZero() {
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = true)
+        )
+        // When the list of tasks is computed with a completed task
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Then the percentages are 100 and 0
+        assertEquals(100f, result.completedTasksPercent)
+        assertEquals(0f, result.activeTasksPercent)
     }
 
     /*
@@ -29,7 +47,8 @@ class StatisticsUtilsTest {
      */
 
     @Test
-    fun getActiveAndCompletedStats_both_returnFortySixty() {
+    fun getActiveAndCompletedStats_both_returnsFortySixty() {
+        // Given 2 completed tasks and 3 active tasks
         val tasks = listOf<Task>(
                 Task("Title", "Description", isCompleted = true),
                 Task("Title", "Description", isCompleted = true),
@@ -37,32 +56,55 @@ class StatisticsUtilsTest {
                 Task("Title", "Description", isCompleted = false),
                 Task("Title", "Description", isCompleted = false)
         )
-
+        // When the list of tasks is computed
         val result = getActiveAndCompletedStats(tasks)
 
+        // Then the result is 40-60
         assertEquals(40f, result.completedTasksPercent)
         assertEquals(60f, result.activeTasksPercent)
     }
 
-    @Test
-    fun getActiveAndCompletedStats_empty_returnsZeros() {
-        val tasks = emptyList<Task>()
+    /*
+    If there's 3 completed task and 2 active task,
+    then there are 40% active tasks and 60% completed tasks.
+     */
 
+    @Test
+    fun getActiveAndCompletedStats_both_returnSixtyForty() {
+        // Given 3 completed tasks and 2 active tasks
+        val tasks = listOf<Task>(
+                Task("Title", "Description", isCompleted = true),
+                Task("Title", "Description", isCompleted = true),
+                Task("Title", "Description", isCompleted = true),
+                Task("Title", "Description", isCompleted = false),
+                Task("Title", "Description", isCompleted = false)
+        )
+        // When the list of tasks is computed
         val result = getActiveAndCompletedStats(tasks)
 
+        // Then the result is 60-40
+        assertEquals(60f, result.completedTasksPercent)
+        assertEquals(40f, result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZeros() {
+        // When there are no tasks
+        val result = getActiveAndCompletedStats(emptyList())
+
+        // Both active and completed tasks are 0
         assertEquals(0f, result.completedTasksPercent)
         assertEquals(0f, result.activeTasksPercent)
     }
 
     @Test
     fun getActiveAndCompletedStats_error_returnsZeros() {
-        val tasks = null
+        // When there's an error loading stats
+        val result = getActiveAndCompletedStats(null)
 
-        val result = getActiveAndCompletedStats(tasks)
-
+        // Both active and completed tasks are 0
         assertEquals(0f, result.completedTasksPercent)
         assertEquals(0f, result.activeTasksPercent)
     }
-
 
 }
