4e186c95a6f7aeede6c15fabf648d7276486de5c
Adrian <ajim1989@gmail.com>
2021-06-02 13:22:55 -0700

    Created setup for activity test by saving task to repo, launching activity, then closing activity before reseeting the db

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
@@ -1,12 +1,16 @@
 package com.example.android.architecture.blueprints.todoapp
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -28,4 +32,17 @@ class TasksActivityTest {
         ServiceLocator.resetRepository()
     }
 
+    @Test
+    fun editTask() = runBlocking {
+        // Set initial state.
+        repository.saveTask(Task("T1", "Task 1"))
+
+        // Start up Tasks screen.
+        val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
+
+        // Espresso code will go here.
+
+        // Make sure the activity is closed before resetting the db:
+        activityScenario.close()
+    }
 }
