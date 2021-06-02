c5cf6ef4045d310eaff490d4e3ffb276668cc453
Adrian <ajim1989@gmail.com>
2021-06-02 14:24:48 -0700

    Added espresso code to click on task, verify info, click on edit, edit info, click on save, verify info change

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
@@ -2,6 +2,12 @@ package com.example.android.architecture.blueprints.todoapp
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.android.architecture.blueprints.todoapp.data.Task
@@ -40,9 +46,26 @@ class TasksActivityTest {
         // Start up Tasks screen.
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
 
-        // Espresso code will go here.
+        // Click on the task on the list and verify that all the data is correct.
+        onView(withText("T1")).perform(click())
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("T1")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("Task 1")))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isNotChecked()))
 
-        // Make sure the activity is closed before resetting the db:
+        // Click on the edit button, edit, save.
+        onView(withId(R.id.edit_task_fab)).perform(click())
+        onView(withId(R.id.add_task_title_edit_text)).perform(replaceText("T2"))
+        onView(withId(R.id.add_task_description_edit_text))
+            .perform((replaceText("Task 2")))
+        onView(withId(R.id.save_task_fab)).perform(click())
+
+        // Verify task is displayed on screen in the task list.
+        onView(withText("T2")).check(matches(isDisplayed()))
+
+        // Verify previous task is not displayed.
+        onView(withText("T1")).check(doesNotExist())
+
+        // Make sure the activity is closed before resetting the db.
         activityScenario.close()
     }
 }
